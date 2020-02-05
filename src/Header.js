1a5eecd0988212639bd2faa7c2ce628414605cfb
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-02-05 12:04:32 +0200

    features, css added

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="logo">Wolt</h1>
+      <div className="searchBar">
+      <input type="text" placeholder="search with street address"></input>
+      </div>
+    </header>
+  )
+}
+
+export {Header as default}
---
+++ src/restaurants.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import './App.css';
+
+
+class Restaurants extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      res: {
+        restaurants: [
+          {
+            name: ''
+          }
+        ]
+      },
+      order: 1
+    }
+
+  }
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/woltapp/summer2020/master/restaurants.json')
+      .then(response => response.json())
+      .then((jsonResponse) => {
+        this.setState({ res: jsonResponse })
+        console.log(this.state.res)
+      }
+      ).catch((err) => {
+        console.log(err)
+      });
+  }
+
+  sortRestaurants = () => {
+    if (this.state.order === 1) {
+      this.sortRestaurantsAlphabetically();
+    }
+    else if (this.state.order === 0) {
+      this.sortRestaurantsReverse();
+    }
+  }
+
+
+  sortRestaurantsReverse = () => {
+    let sortedArray;
+    sortedArray = this.state.res.restaurants.sort(function (a, b) {
+      if (a.name > b.name) return -1;
+      else if (a.name < b.name) return 1;
+      return 0;
+    })
+    this.setState({ res: { restaurants: sortedArray }, order: 1 });
+  }
+
+  sortRestaurantsAlphabetically = () => {
+    let sortedArray;
+    sortedArray = this.state.res.restaurants.sort(function (a, b) {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      return 0;
+    })
+    this.setState({ res: { restaurants: sortedArray }, order: 0 });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <button className="button" onClick={this.sortRestaurants}>Sort restaurants </button>
+        </div>
+        <div className="card">
+          {this.state.res.restaurants.map(function (restaurant, idx) {
+            return (
+              <Card key={idx}>
+                <CardContent className="card-content">
+
+                  <img
+                    src={restaurant.image}
+                    className="media"
+                  />
+                  <CardContent>
+                                       
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {restaurant.name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {restaurant.description}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="h6">{restaurant.city}</Typography>
+                 
+                  </CardContent>
+
+                </CardContent>
+              </Card>
+            )
+          })}
+
+        </div>
+      </div>
+    );
+
+  }
+
+}
+// {this.state.res.restaurants.map(function (restaurant, idx) {
+//   return (
+//     <li className="list" key={idx}>{restaurant.name} <img src={restaurant.image}></img></li>
+//     )
+// })}
+
+
+export { Restaurants as default }
+
